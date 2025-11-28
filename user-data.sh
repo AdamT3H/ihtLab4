6fdcc21f75b8c508f69cf99b773b6ffb815e0d8f
AdamT3H <144066033+AdamT3H@users.noreply.github.com>
2025-11-28 20:34:50 +0200

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -15,6 +15,7 @@ adduser --disabled-password --gecos "" poweruser
 IPT=$(which iptables)
 echo "poweruser ALL=(root) NOPASSWD: $IPT" >> /etc/sudoers
 
-sudo setfacl -m u:poweruser:rx /home/adminuser
+chown adminuser:adminuser /home/adminuser
+setfacl -m u:poweruser:rx /home/adminuser
 
 sudo -u poweruser ln -s /etc/mtab /home/poweruser/mtab_link
