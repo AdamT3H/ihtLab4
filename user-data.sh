e2d8db1c4e17a13ff38ab8d4681634a48598dd1e
AdamT3H <144066033+AdamT3H@users.noreply.github.com>
2025-12-01 10:26:59 +0200

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-adduser -s /bin/bash --disabled-password --gecos "" adminuser
+adduser -s -m /bin/bash --disabled-password --gecos "" adminuser
 
 usermod -aG sudo adminuser
 
@@ -8,16 +8,19 @@ HASHED=$(echo -n "PASSWORD" | openssl passwd -6 -stdin)
 
 echo "adminuser:$HASHED" | chpasswd -e
 
+echo "adminuser ALL =(ALL:ALL) ALL" | tee /etc/sudoers.d/adminuser
+chmod 440 /etc/sudoers.d/adminuser
+
 adduser -s /bin/bash --gecos "" poweruser
 passwd -d poweruser
 
 IPT=$(which iptables)
-echo "poweruser ALL=(root) NOPASSWD: $IPT" > /etc/sudoers.d/poweruser
-
-chmod 440 /etc/sudoers.d/poweruser
+echo "poweruser ALL=(root) NOPASSWD: $IPT" > /etc/sudoers.d/poweruser-iptables
+chmod 440 /etc/sudoers.d/poweruser-iptables
 
 usermod -aG adminuser poweruser
 
+chown -R adminuser:adminuser /home/adminuser
 chmod 750 /home/adminuser
 
 ln -s /etc/mtab /home/poweruser/mtab_link
